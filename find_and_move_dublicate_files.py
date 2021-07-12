fd8b1325ca2edc98f039f5f76fca1c6b6879da38
root <root@localhost.localdomain>
2021-07-12 16:59:53 +0300

    add ui

--- find_and_move_dublicate_files.py
+++ find_and_move_dublicate_files.py
@@ -2,17 +2,22 @@
 import os
 import filecmp
 import shutil
+import tkinter as tk
+from tkinter import filedialog as fd
+import time
 
-def create_files_dict():
-    print('\nstart create files dict... ')
+def create_files_dict(): 
+    path = dict_folders.get('source_directory')
+    lbl_window_state_pole.configure(text='Start Create Files Dict...',bg='green')
+    lbl_window_state_pole.update()
     for dirpath, dirnames,filenames in os.walk(path):
         for name in filenames:
             if name not in dict_files:
                 dict_files[name] = list()
             dict_files[name].append(os.path.join(dirpath, name))
-    print('\nend create files dict...')
+    lbl_window_state_pole.configure(text='End Create Files Dict...',bg='green')
+    lbl_window_state_pole.update()
     files_count = len(dict_files)
-    print(f'\nall find files...{files_count}')
 
 def compared_two_files(file_1, file_2):
     return filecmp.cmp(file_1, file_2)
@@ -40,30 +45,84 @@ def comparsion_file_list(file_list):
 def find_dublicate_files_func():
     for key in dict_files:
         comparsion_file_list(dict_files[key])
-    files_to_transfer = len(set_subject_to_transfer)
-    print(f'\nneed move files...{files_to_transfer}')
+    lbl_dublicate_files_pole.configure(text=len(set_subject_to_transfer))
+    lbl_dublicate_files_pole.update()
     
-def move_files_func(numerate=1):
-    print('start file move...')
+def move_files_func(numerate=0):
+    path_to_destination = dict_folders['dublicate_folder']
+    files_to_transfer = len(set_subject_to_transfer)
+    lbl_window_state_pole.configure(text='Start File Move...',bg='green')
+    lbl_window_state_pole.update()
     for file_move in set_subject_to_transfer:
-        print_func(file_move)
-        print()
         new_file_name =  path_to_destination + str(numerate) + '_' + file_move.split('/')[-1]
         shutil.move(file_move, new_file_name)
         numerate += 1
-    print('end files move...')
+        lbl_moving_file.configure(text=file_move.split('/')[-1])
+        lbl_moving_file.update()
+        lbl_files_remaining_pole.configure(text= files_to_transfer - numerate)
+        lbl_files_remaining_pole.update()
+    lbl_window_state_pole.configure(text='End Files Move...',bg='green')
+    lbl_window_state_pole.update()
 
-def print_func(data=''):
-    print(data+'\r', end='')
-#
-path = '' # path source folder
-path_to_destination = '' # path destination folder
+def set_source_directory():
+    folder_name = fd.askdirectory()
+    dict_folders['source_directory']=folder_name+str('/')
+    lbl_source_directory.configure(text='['+folder_name.split('/')[-1]+']')
+
+def set_dublicate_folder():
+    folder_name=fd.askdirectory()
+    dict_folders['dublicate_folder']=folder_name+str('/')
+    lbl_destination_folder.configure(text='['+folder_name.split('/')[-1]+']')
+
+def func_start():
+    lbl_window_state_pole.configure(text='Start Programm',bg='green')
+    lbl_window_state_pole.update()
+    path = dict_folders.get('source_directory')
+    path_to_destination = dict_folders.get('dublicate_folder')
+    if not path or not path_to_destination:
+        lbl_window_state_pole.configure(text='Error, Path is Empty',bg='red')
+        lbl_window_state_pole.update()
+        return
+    else:
+        lbl_window_state_pole.configure(text='Start Programm',bg='green')
+        lbl_window_state_pole.update()
+        create_files_dict()
+        find_dublicate_files_func()
+        move_files_func()
 
 dict_files = dict()
 set_compared = set()
 set_subject_to_transfer = set()
+dict_folders=dict()
+
+okno = tk.Tk()
+okno.title('Welcome')
+okno.geometry('500x350')
+btn_1=tk.Button(okno,text='Choice Source Folder ->',command=set_source_directory)
+btn_2=tk.Button(okno,text='Choice Dublicate Folder ->',command=set_dublicate_folder)
+btn_1.grid(column=0,row=0,ipadx=20,pady=10)
+btn_2.grid(column=0,row=2,ipadx=12,pady=10)
+lbl_source_directory=tk.Label(okno)
+lbl_source_directory.grid(column=1,row=0,padx=10)
+lbl_destination_folder=tk.Label(okno)
+lbl_destination_folder.grid(column=1,row=2,padx=10)
+lbl_window_state = tk.Label(okno, text='Programm State:')# okno sostianija
+lbl_window_state.grid(column=0,row=4,pady=10)
+lbl_window_state_pole=tk.Label(okno,text='State Programm Now')
+lbl_window_state_pole.grid(column=1,row=4,pady=10)
+lbl_dublicate_files = tk.Label(okno,text='All Dublicate Files:')
+lbl_dublicate_files.grid(column=0,row=6,pady=10)
+lbl_dublicate_files_pole = tk.Label(okno)
+lbl_dublicate_files_pole.grid(column=1,row=6)
+lbl_files_remaining = tk.Label(okno,text='Files Remaining:')
+lbl_files_remaining.grid(column=0,row=8,pady=10)
+lbl_files_remaining_pole=tk.Label(okno)
+lbl_files_remaining_pole.grid(column=1,row=8,pady=10)
+lbl_moving_file=tk.Label(okno)
+lbl_moving_file.grid(column=0,row=10,columnspan=2,pady=10)
+btn_start=tk.Button(okno,text='Start',command=func_start)
+btn_start.grid(column=0,row=13,pady=10, ipadx=30,ipady=10)
+exit_btn=tk.Button(okno,text='Exit',comman=exit)
+exit_btn.grid(column=1,row=13,pady=10, ipadx=30,ipady=10)
+okno.mainloop()
 #
-if __name__ == '__main__':
-    create_files_dict()
-    find_dublicate_files_func()
-    move_files_func()
